02ad08855c022815dbbcad82cd18005aa6f4fc9d
Andreas Lamert <andrei14la@hotmail.com>
2023-10-01 21:33:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+    function func() {
+    let array = ["London", "Paris",
+        "Berlin","Madrid"]
+   
+    
+    let new_array = array.reverse();
+    console.log(new_array);
+   }
+   func();
+   
